4062be3c68f4dac357031adce3433c85346e02c8
Adetutu <adepojuadetutu006@gmail.com>
2022-08-20 12:35:06 +0100

    React useState

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+
+export default function App() {
+
+  const [count, setCount ] = useState(0)
+
+  return (
+    <div>
+      <h1>My Count is { count }</h1>
+      <button onClick={()=>setCount(count+1)}>Add Count</button>
+    </div>
+  );
+}
